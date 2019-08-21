92da6955c8deb04bf7d1114cdf46d3b11fd16ecb
Nathaniel Knapp <nathaniel.knapp@gmail.com>
2019-08-20 17:38:42 -0800

    better files

--- reorganize_tsv.py
+++ reorganize_tsv.py
@@ -1,17 +1,22 @@
 csv_file_name = './lgaddr.csv'
 better_file_name = './lgaddr_corrected.csv'
-handle = open(tsv_file_name, 'r')
+handle = open(csv_file_name, 'r')
 lines = handle.readlines()[1:]
 
-lga_dict = {}
+lga_list = list()
 
 for line in lines:
   split_line = line.split(',')
+  if len(split_line[2]) < 2:
+    continue
   legislator = split_line[1]
   address_number = split_line[2].split()[0]
-  street_name = split_line[2].split()[1:].join(' ')
-  print(legislator, address_number, street_name) 
- 
+  street_name = ' '.join(split_line[2].split()[1:])
+  lga_list.append(legislator + ',' + address_number[1:] + ',' + street_name + '\n')
+
+handle = open(better_file_name, 'w')
+for lga in lga_list:
+  handle.write(lga)   
 
 
 
