702539165c0a64238846a88ee74d633767c76229
Nathaniel Knapp <nathaniel.knapp@gmail.com>
2019-08-25 16:46:32 -0800

    backup

--- show_legislators.py
+++ show_legislators.py
@@ -1,4 +1,5 @@
 import geopandas
+from geopy.geocoders import Nominatim
 
 legislator_file = 'lgaddr_corrected.csv'
 handle = open(legislator_file, 'r')
@@ -11,12 +12,25 @@ for line in lines:
   if len(split_line) == 3:
     leg_dict[split_line[0]] = [split_line[1], split_line[2][:-1]]
 
+# read existing coordinates from file
+handle = open('leg_coord_dict.csv', 'r'   
+
+leg_coord_dict = {}
+lines = handle.readlines()
+
+
 # get coordinates for addresses
-
-
-
-
-
-# plot coordinates
-
+geolocator = Nominatim(user_agent='ak_redistrict')
+for leg in leg_dict:
+  if leg not in leg_coord_dict:
+    leg_address = leg_dict[leg][0] + ' ' + leg_dict[leg][1]
+    try:
+      leg_location = geolocator.geocode(leg_address)
+    except:
+      continue
+  if leg_location is not None:
+    print(leg_location)
+    leg_coord_dict[leg] = [leg_location.latitude, leg_location.longitude]
+ 
+print(leg_coord_dict)
  
