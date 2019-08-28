a2f0b701fd4b6228df053d0be958955a5357f3e2
Nathaniel Knapp <nathaniel.knapp@gmail.com>
2019-08-28 10:27:44 -0800

    backup

--- show_anchorage.py
+++ show_anchorage.py
@@ -3,20 +3,26 @@ import matplotlib.pyplot as plt
 import pandas
 import os
 
-os.system('SHAPE_RESTORE_SHX=YES fio info gz_2010_us_050_00_20m.shp')
+os.system('SHAPE_RESTORE_SHX=YES fio info shapefiles/2013_districts.shp')
 
-borough_file = 'gz_2010_us_050_00_20m.shp'
-full_file = os.path.join(os.getcwd(), borough_file)
-boroughs = geopandas.read_file(full_file) 
-borough_geometry = boroughs['geometry']
+#borough_file = 'shapefiles/boroughs.shp'
+#precinct_file = 'shapefiles/2013_precincts.shp'
+district_file = 'shapefiles/2013_districts.shp'
+full_file = os.path.join(os.getcwd(), district_file)
+df = geopandas.read_file(full_file) 
+geometry = df['geometry']
+plot = geometry.plot(color='white', edgecolor='black')
 
-borough_plot = borough_geometry.plot(color='white', edgecolor='black')
-gdf = geopandas.GeoDataFrame(boroughs, geometry=borough_geometry) 
+gdf = geopandas.GeoDataFrame(df, geometry=geometry) 
 
-gdf.plot(ax=borough_plot, color='white')
+gdf.plot(ax=plot, color='white', edgecolor='black')
 
-plt.ylim(58, 70)
-plt.xlim(-160, -140)
+anchorage_min = 61.0
+anchorage_max = 61.24
+anchorage_min_long = -150.03
+anchorage_max_long = -149.7
+#plt.ylim(anchorage_min, anchorage_max)
+#plt.xlim(anchorage_min_long, anchorage_max_long)
 plt.show()
 
 #borough_plot.get_figure().savefig('/Users/nknapp/Desktop/test.pdf')
