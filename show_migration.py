1e6e5e323855491c4d507fb4edeaf2c82ca8ad5f
Nathaniel Knapp <nathaniel.knapp@gmail.com>
2019-08-27 20:15:05 -0800

    backup

--- show_migration.py
+++ show_migration.py
@@ -1,10 +1,26 @@
 import geopandas
-import geoplot
+import matplotlib.pyplot as plt
+import pandas
+import os
 
-boroughs = geopandas.read_file('borough/mv_borough_py.shp')
+os.system('SHAPE_RESTORE_SHX=YES fio info gz_2010_us_050_00_20m.shp')
+
+borough_file = 'gz_2010_us_050_00_20m.shp'
+full_file = os.path.join(os.getcwd(), borough_file)
+boroughs = geopandas.read_file(full_file) 
 borough_geometry = boroughs['geometry']
-print(borough_geometry)
-print(boroughs)
-borough_plot = boroughs.plot(color='white', edgecolor='black')
-borough_plot.get_figure().savefig('/Users/nknapp/Desktop/test.pdf')
+
+borough_plot = borough_geometry.plot(color='white', edgecolor='black')
+gdf = geopandas.GeoDataFrame(boroughs, geometry=borough_geometry) 
+
+gdf.plot(ax=borough_plot, color='white')
+
+plt.ylim(58, 70)
+plt.xlim(-160, -140)
+plt.show()
+
+#borough_plot.get_figure().savefig('/Users/nknapp/Desktop/test.pdf')
+
+
+
 
