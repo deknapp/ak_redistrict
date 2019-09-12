a9334f5b0f9d1a3dad40f909ce3898b97c5abb95
Nathaniel Knapp <nathaniel.knapp@gmail.com>
2019-09-12 11:13:39 -0800

    fix read script for migration files

--- get_migration_2010_2016.py
+++ get_migration_2010_2016.py
@@ -6,5 +6,15 @@ def get_file_for_years(yearA, yearB):
 def migration_dict(yearA, yearB):
   return migration.read_file(get_file_for_years(yearA, yearB))
 
-dct = migration_dict(2010, 2011)
+dict_2010_2011 = migration_dict(2010, 2011)
+
+for key in dict_2010_2011:
+  print(key)
+  print('------------------------')
+  for k in dict_2010_2011[key]:
+    val = dict_2010_2011[key][k]
+    if val != '-':
+      print(val)
+  
+
 
--- migration.py
+++ migration.py
@@ -11,19 +11,31 @@ def read_file(file_name):
   full_file_name = os.path.join(os.getcwd(), MIGRATION_CSV_DIRECTORY, file_name)
   handle = open(full_file_name, 'r')
   lines = handle.readlines()
-  header_line = lines[HEADER_LINE-1]
-  to_names = header_line.split(',')[2:-2]
+  header_line = lines[CHANGE_HEADER_LINE-1]
+  split_header_line = header_line.split(',')
   to_from_dict = {}
-  for name in to_names:
-    to_from_dict[name] = {}
-  for line in lines[FIRST_CHANGE_LINE-1, LAST_CHANGE_LINE-1]:
-    split_line = line.split(',')
+  for line in lines[FIRST_CHANGE_LINE-1:LAST_CHANGE_LINE-1]:
+    quote_split_line = line.split('"')
+    i = 0
+    cleaned_line = ''
+    for chunk in quote_split_line:
+      if i%2 == 0:
+        cleaned_line += chunk
+      else:
+        cleaned_line += chunk.replace(',',"")
+      i = i+1 
+    split_line = cleaned_line.split(',')
     area_name = split_line[0]
-    from_name = area_name.split('in')[-1]
+    from_name = area_name.split('in')[1]
+    if len(area_name.split('in')) > 2:     
+      for name_part in area_name.split('in')[2:]:
+        from_name = from_name + 'in' + name_part
     i = 2
-    for number in split_line[2:-2]:
-      to_from_dict[header_line[i]] = number
+    to_from_dict[from_name] = dict()
+    for number in split_line[2:-3]:
+      if i > len(split_header_line) - 1:
+        break
+      to_from_dict[from_name][split_header_line[i]] = number
       i = i + 1
-  print(to_from_dict)
   return to_from_dict
 
