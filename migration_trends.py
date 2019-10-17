aa6046edcc38527b94ccb88a130a4fd4969df02f
Nathaniel Knapp <nathaniel.knapp@gmail.com>
2019-10-17 14:24:53 -0800

    pie chart code

--- migration_trends.py
+++ migration_trends.py
@@ -7,7 +7,6 @@ def get_net_migration_list(first_year, last_year, dest, source):
   lst = []
   for i in range(first_year, last_year):
     dct = migration.migration_dict(i, i+1)
-    print(dct[names.ANCHORAGE][names.OUT_OF_STATE])
     to_val = 0
     try:
       to_val = dct[source][dest] 
@@ -38,7 +37,8 @@ def plot_migration(frm, to, start_year, end_year):
     os.mkdir('/Users/nknapp/Desktop/akpirg/migration_plots/migration_to_' + to + '/')
   except:
     pass
-  plt.savefig('/Users/nknapp/Desktop/akpirg/migration_plots/migration_to_' + to + '/migration_from_' + frm + '_to_' + to + '_' + str(start_year) + '_' + str(end_year) + '.png') 
+  #plt.show()
+  #plt.savefig('/Users/nknapp/Desktop/akpirg/migration_plots/migration_to_' + to + '/migration_from_' + frm + '_to_' + to + '_' + str(start_year) + '_' + str(end_year) + '.png') 
 
 def plot_all_migration(start_year, end_year):
   for frm in names.PLACE_LIST:
@@ -46,5 +46,51 @@ def plot_all_migration(start_year, end_year):
       if frm != to:
         plot_migration(frm, to, start_year, end_year)
 
-plot_migration(names.ANCHORAGE, names.OUT_OF_STATE, 2010, 2016)
+def pie_chart_migration(to, start_year, end_year):
+  inflow_dct = dict()
+  outflow_dct = dict() 
+  for frm in names.PLACE_LIST:
+    if frm != to:
+      total = sum(get_net_migration_list(start_year, end_year, to, frm))
+      if total > 0:
+        outflow_dct[frm] = total
+      elif total < 0:
+        inflow_dct[frm] = total
+  print("INFLOW")
+  print(inflow_dct)
+  print("OUTFLOW")
+  print(outflow_dct)    
+  plt.clf()
+  plt.rcParams["font.size"] = 8
+  plt.title('Population inflow to ' + to + ' since '+ str(start_year))
+  labels = inflow_dct.keys()
+  values = inflow_dct.values()
+  ten_percent_value = sum(values)/10.0
+  end_labels = [label for label in labels if (inflow_dct[label] > ten_percent_value)]
+  end_val = [val for val in values if val > ten_percent_value]
+  fig1, ax1 = plt.subplots()
+  ax1.pie(values, labels=labels)
+  ax1.axis('equal')
+  plt.savefig('/Users/nknapp/Desktop/akpirg/pie_charts/inflow_to_' + to + '.png')
+  plt.clf()
+  plt.rcParams["font.size"] = 8
+  plt.title('Population outflow from ' + to + ' since '+ str(start_year))
+  labels = outflow_dct.keys()
+  values = outflow_dct.values()
+  ten_percent_value = sum(values)/10.0
+  end_labels = [label for label in labels if outflow_dct[label] > ten_percent_value]
+  end_val = [val for val in values if val > ten_percent_value]
+  fig1, ax1 = plt.subplots()
+  ax1.pie(end_val, labels=end_labels)
+  ax1.axis('equal')
+  plt.savefig('/Users/nknapp/Desktop/akpirg/pie_charts/outflow_from_' + to + '.png')
+
+
+def plot_all_migration(start_year, end_year):
+  for to in names.PLACE_LIST:
+    pie_chart_migration(to, start_year, end_year)
+
+pie_chart_migration(names.MATSU, 2010, 2016)
+
+#plot_migration(names.ANCHORAGE, names.OUT_OF_STATE, 2010, 2016)
 #plot_all_migration(2010, 2016) 
