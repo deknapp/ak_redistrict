03f31c286ec08667d83663d9373be6b2ad5f5a2b
Nathaniel Knapp <nathaniel.knapp@gmail.com>
2019-10-23 23:30:23 -0800

    backup

--- migration_trends.py
+++ migration_trends.py
@@ -23,38 +23,33 @@ def get_net_migration_list(first_year, last_year, dest, source):
     lst.append(net_val)
   return lst
 
-def compare_migration(start_year, end_year, from_list, to):
-  plt.clf()
-  plt.title('Rural migration to ' + to + ' since '+ str(start_year))
-  plt.ylabel('Net population flow')
-  plt.xlabel('Year')
-  x_vals = range(start_year, end_year+1)
-  for frm in from_list:
-    lst = get_net_migration_list(start_year, end_year, frm, to)
-    y_vals = [0]
-    for val in lst:
-      y_vals.append(y_vals[-1] + val)
-    plt.plot(x_vals, y_vals, label=frm)
-  plt.legend(loc='upper left')
-  try:
-    os.mkdir('/Users/nknapp/Desktop/akpirg/migration_compare_plots/')
-  except:
-    pass
-  plt.savefig('/Users/nknapp/Desktop/akpirg/migration_compare_plots/migration_to_' + to + '_' + str(start_year) + '_' + str(end_year) + '.png') 
+def get_combined_migration_list(first_year, last_year, dest, sources):
+  sum_lst = [] 
+  for source in sources:
+    new_lst = get_net_migration_list(first_year, last_year, dest, source)
+    if len(sum_lst) > 0:
+      sum_lst = [sum(x) for x in zip(sum_lst, new_lst)] 
+    else:
+      sum_lst = new_lst
+  return sum_lst
 
-# NTK TODO:
-def compare_sums_of_migration(start_year, end_year, from_list, to):
+def compare_migration(start_year, end_year, from_list, to, frm_label=None):
   plt.clf()
-  plt.title('Rural migration to ' + to + ' since '+ str(start_year))
+  plt.title('Rural migration to ' + to + ' ' +  str(start_year) + '-' + str(end_year))
   plt.ylabel('Net population flow')
   plt.xlabel('Year')
   x_vals = range(start_year, end_year+1)
   for frm in from_list:
-    lst = get_net_migration_list(start_year, end_year, frm, to)
+    frm_label = frm 
+    if isinstance(frm, list):
+      lst = get_combined_migration_list(start_year, end_year, frm, to)
+      frm_label = frm_label
+    else:
+      lst = get_net_migration_list(start_year, end_year, frm, to)
     y_vals = [0]
     for val in lst:
       y_vals.append(y_vals[-1] + val)
-    plt.plot(x_vals, y_vals, label=frm)
+    plt.plot(x_vals, y_vals, label=frm_label)
   plt.legend(loc='upper left')
   try:
     os.mkdir('/Users/nknapp/Desktop/akpirg/migration_compare_plots/')
@@ -89,7 +84,7 @@ def plot_all_migration(start_year, end_year):
 
 #compare_migration(2010, 2016, from_list_matsu, names.MATSU)
 
-#from_list_anchorage = [names.MATSU, names.FAIRBANKS, names.JUNEAU, names.OUT_OF_STATE]
+#from_list_anchorage = [names.MATSU, names.FAIRBANKS, names.JUNEAU, names.OUT_OF_STATE, names.]
 #compare_migration(2010, 2016, from_list_anchorage, names.ANCHORAGE)
 
 #from_list_fairbanks = [names.MATSU, names.ANCHORAGE, names.JUNEAU, names.OUT_OF_STATE]
@@ -98,6 +93,6 @@ def plot_all_migration(start_year, end_year):
 #from_list_juneau = [names.MATSU, names.ANCHORAGE, names.JUNEAU, names.OUT_OF_STATE]
 #compare_migration(2010, 2016, from_list_juneau, names.JUNEAU)
 
-#from_list_rural = [names.YUKON, names.NW_ARCTIC, names.NORTH_SLOPE, names.NOME, names.DENALI] #, names.ALEUTIANS_EAST, names.ALEUTIANS_WEST]
-#compare_migration(2010, 2016, from_list_rural, names.ANCHORAGE)
-#compare_migration(2010, 2016, from_list_rural, names.FAIRBANKS) 
+compare_migration(2010, 2016, names.RURAL_BIG, names.ANCHORAGE)
+compare_migration(2010, 2016, names.RURAL_NORTHERN, names.FAIRBANKS) 
+compare_migration(2010, 2016, names.RURAL_SOUTHEAST, names.JUNEAU) 
