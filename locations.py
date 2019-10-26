3ea0c30668fade11bec08eaaa1f443815d9b7d84
Nathaniel Knapp <nathaniel.knapp@gmail.com>
2019-10-26 10:30:53 -0800

    add coords class

--- locations.py
+++ locations.py
@@ -20,4 +20,18 @@ JUNEAU_MIN_LONG = -134
 JUNEAU_MIN_LAT = 58
 JUNEAU_MAX_LAT = 58.6
 
+def Coords:
+  def __init__(self, min_lat=None, max_lat=None, min_long=None, max_long=None):
+    self.min_lat = min_lat
+    self.max_lat = max_lat
+    self.min_long = min_long
+    self.max_long = max_long
+
+ANCH_COORDS = Coords(min_lat=ANCH_MIN_LAT, max_lat=ANCH_MAX_LAT, min_long=ANCH_MIN_LONG, max_long=ANCH_MAX_LONG)
+MATSU_COORDS = Coords(min_lat=MATSU_MIN_LAT, max_lat=MATSU_MAX_LAT, min_long=MATSU_MIN_LONG, max_long=MATSU_MAX_LONG)
+JUNEAU_COORDS = Coords(min_lat=JUNEAU_MIN_LAT, max_lat=JUNEAU_MAX_LAT, min_long=JUNEAU_MIN_LONG, max_long=JUNEAU_MAX_LONG)
+FAIRBANKS_COORDS = Coords(min_lat=FAIRBANKS_MIN_LAT, max_lat=FAIRBANKS_MAX_LAT, min_long=FAIRBANKS_MIN_LONG, max_long=FAIRBANKS_MAX_LONG)
+
+
+
 
